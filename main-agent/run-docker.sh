e28525dc5924c4b3af817fc76522e294f23a677c
spen428 <spen428@users.noreply.github.com>
2023-06-24 10:46:52 +0100

    Give up using the minimal image

--- main-agent/run-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# https://github.com/JetBrains/teamcity-docker-agent/blob/master/run-docker.sh
-if [ "$DOCKER_IN_DOCKER" = "start" ] ; then
- rm /var/run/docker.pid 2>/dev/null
- service docker start
- echo "Docker daemon started"
-fi
